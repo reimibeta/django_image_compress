c70da0fea9049968c71614b91d9519c83daeb773
reimi <65539218+reimibeta@users.noreply.github.com>
2023-06-05 17:26:20 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_image_compress',
-    version='0.0.1',
+    version='1.0.0',
     description='A sample image compress',
     long_description='Test Long',
     author='ReimiBeta',
